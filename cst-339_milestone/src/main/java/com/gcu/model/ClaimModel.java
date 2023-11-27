b880fd3915114545720a7df78beb916a6372e488
hbece <hbece@Laptop-Becerra>
2023-11-26 21:32:42 -0500

    Update: ClaimModel.java implemented

--- cst-339_milestone/src/main/java/com/gcu/model/ClaimModel.java
+++ cst-339_milestone/src/main/java/com/gcu/model/ClaimModel.java
@@ -1,5 +1,81 @@
 package com.gcu.model;
 
+import java.time.LocalDate;
+
 public class ClaimModel {
 
+	private String claimId;
+    private String patientName;
+    private String diagnosis;
+    private LocalDate claimDate;
+    private double claimAmount;
+
+    // Constructors, getters, and setters
+
+    public ClaimModel() {
+        // Default constructor
+    }
+
+    public ClaimModel(String claimId, String patientName, String diagnosis, LocalDate claimDate, double claimAmount) {
+        this.claimId = claimId;
+        this.patientName = patientName;
+        this.diagnosis = diagnosis;
+        this.claimDate = claimDate;
+        this.claimAmount = claimAmount;
+    }
+
+    // Getter and setter methods
+
+    public String getClaimId() {
+        return claimId;
+    }
+
+    public void setClaimId(String claimId) {
+        this.claimId = claimId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public LocalDate getClaimDate() {
+        return claimDate;
+    }
+
+    public void setClaimDate(LocalDate claimDate) {
+        this.claimDate = claimDate;
+    }
+
+    public double getClaimAmount() {
+        return claimAmount;
+    }
+
+    public void setClaimAmount(double claimAmount) {
+        this.claimAmount = claimAmount;
+    }
+
+    // toString method for better logging or debugging
+
+    @Override
+    public String toString() {
+        return "ClaimModel{" +
+                "claimId='" + claimId + '\'' +
+                ", patientName='" + patientName + '\'' +
+                ", diagnosis='" + diagnosis + '\'' +
+                ", claimDate=" + claimDate +
+                ", claimAmount=" + claimAmount +
+                '}';
+    }
 }
