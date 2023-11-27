8ce10359ac9d2a8ee69a666681c6557272ac9868
hbece <hbece@Laptop-Becerra>
2023-11-27 02:50:11 -0500

    footer copyright update

--- cst-339_milestone/src/main/java/com/gcu/controller/ClaimController.java
+++ cst-339_milestone/src/main/java/com/gcu/controller/ClaimController.java
@@ -3,13 +3,18 @@ package com.gcu.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gcu.model.ClaimModel;
 import com.gcu.repository.ClaimCollectionRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/claims")
@@ -40,6 +45,22 @@ public class ClaimController {
 		
 		return modelAndView;
 	}
+	
+	@PostMapping("/createClaim")
+	public String createClaim(@Valid ClaimModel claimModel, BindingResult bindingResult, Model model) {
+		// Check for validation errors
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("title", "New Claim");
+			return "claimNew";
+		}
+
+		// For now, you can print the user details to the console
+		System.out.println(claimModel.toString());
+		model.addAttribute("title", "Claims");
+		
+		return "claim"; // Redirect to a success page
+	}
+	
 	
 	public List<ClaimModel> findAll() {
 		return claims.findAll();
