e6e0bb997557c5f3640e3798d51213f81b64d426
hbece <hbece@Laptop-Becerra>
2023-11-29 12:50:32 -0500

    Test push on Test Branch

--- cst-339_milestone/src/main/java/com/gcu/Cst339MilestoneApplication.java
+++ cst-339_milestone/src/main/java/com/gcu/Cst339MilestoneApplication.java
@@ -8,6 +8,10 @@ public class Cst339MilestoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cst339MilestoneApplication.class, args);
+		
+		/**
+		 * Test
+		 */
 	}
 
 }
